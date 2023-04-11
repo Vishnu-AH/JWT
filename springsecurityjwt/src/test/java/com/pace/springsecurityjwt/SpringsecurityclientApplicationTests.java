b2f16c9c08b56faacf8c26b8e2109aa974e59c62
Vishnu-AH <122531074+Vishnu-AH@users.noreply.github.com>
2023-04-11 18:28:42 +0530

    Add files via upload

---
+++ springsecurityjwt/src/test/java/com/pace/springsecurityjwt/SpringsecurityclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pace.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
